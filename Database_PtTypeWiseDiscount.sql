07bc7a2b0bb21b9b4872726dbaa28d2398a3211c
Gade Rushikesh <143445674+GadeRushi0202@users.noreply.github.com>
2024-10-29 15:48:17 +0530

    Add files via upload

---
+++ Database_PtTypeWiseDiscount.sql
@@ -0,0 +1,26 @@
+create table mst_PtType(
+PtTypeId int primary key identity(1,1),
+PtType nvarchar(50),
+)
+ALTER TABLE mst_PtType
+ADD IsActive bit
+
+create table mst_PtTypeWiseDiscount(
+Id int primary key identity(1,1),
+OpIpType bit,
+PtTypeId int,
+Constraint fk_PtType foreign key(PtTypeId) references mst_PtType(PtTypeId)
+)
+
+ALTER TABLE mst_PtTypeWiseDiscount
+ADD Discount Int , IsActive BIT;
+
+ALTER TABLE mst_PtType
+ADD IsActive bit
+
+select * from mst_PtTypeWiseDiscount
+select * from mst_PtType
+
+ 
+
+truncate table mst_PtType
